f4756308f60d4d8bbd045f6fdb1dc9bed16e8268
MaisUmHugo <149391609+MaisUmHugo@users.noreply.github.com>
2026-03-31 22:46:48 -0300

    Feat: Shooter

---
+++ ClothSeminario/Assets/Scripts/Shooter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _maxDistance = 100f;
+    [SerializeField] private LayerMask _hitMask;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (_camera == null)
+            _camera = Camera.main;
+
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance, _hitMask))
+        {
+            ClothTarget target = hit.collider.GetComponentInParent<ClothTarget>();
+
+            if (target != null)
+            {
+                target.Hit(hit.point, ray.direction);
+            }
+        }
+    }
+}
