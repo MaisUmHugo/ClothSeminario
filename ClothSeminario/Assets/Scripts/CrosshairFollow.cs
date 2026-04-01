680b2b34b963c54a7384040789ee0487e8dbfdce
jprvivasGS <jprvivas@gmail.com>
2026-04-01 18:37:21 -0300

    FEAT: roupas ajustadas e mira implementada

--- ClothSeminario/Assets/Scripts/CrosshairFollow.cs
+++ ClothSeminario/Assets/Scripts/CrosshairFollow.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 
-public class CrosshairFollow : MonoBehaviour
-{
-    void Update()
-    {
+public class CrosshairFollow : MonoBehaviour{
+
+    private void Start() {
+        Cursor.visible = false;
+    }
+
+    void Update(){
         transform.position = Input.mousePosition;
     }
 }
